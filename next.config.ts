091d3e80171a95ca0e261d72cf88e6aa3109d3c3
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2026-01-11 23:11:46 +0900

    config: next/image 외부 이미지 호스트 제한 해제

--- next.config.ts
+++ next.config.ts
@@ -4,21 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.sejongssg.kr',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
