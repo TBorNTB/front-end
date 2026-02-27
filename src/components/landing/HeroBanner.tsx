fe4698ca1badd7b89523f9392a42301f70e4cdde
kaydeeloka <nurulhudaalifi@gmail.com>
2026-02-28 02:20:06 +0900

    update button apply ssg

--- src/components/landing/HeroBanner.tsx
+++ src/components/landing/HeroBanner.tsx
@@ -64,11 +64,11 @@ export default function HeroBanner() {
           </Link>
           
           <Link 
-            href="/community"
+            href="https://ssg.sejong.ac.kr/apply/#contact"
             className="bg-black/70 px-6 py-3 rounded-lg font-bold text-base transition-all shadow-lg glow-button-secondary flex items-center justify-center space-x-2 border-2 border-primary-500 text-secondary-300 shadow-primary-500/20 hover:bg-black/50 hover:border-primary-600 hover:shadow-primary-500/40"
           >
             <Code size={20} />
-            <span>커뮤니티 보기</span>
+            <span>SSG 지원하기</span>
           </Link>
         </div>
       </div>
