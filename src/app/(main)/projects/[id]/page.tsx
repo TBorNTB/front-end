b10c51116e7c00b6541f1d18f1c137552cd1414b
sangkyu39 <sangkyu.p39@gmail.com>
2026-05-13 15:39:02 +0900

    [FE] 도큐멘트 작성 오타 수정

--- src/app/(main)/projects/[id]/page.tsx
+++ src/app/(main)/projects/[id]/page.tsx
@@ -1818,7 +1818,7 @@ export default function ProjectPage({ params }: ProjectPageProps) {
                           <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                           </svg>
-                          새 도큐멘트 착성
+                          새 도큐멘트 작성
                         </button>
                       </div>
                     </div>
